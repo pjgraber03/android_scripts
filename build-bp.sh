049e9b318051b0da9d38337b7a42669ddb305654
Patrick Graber <pjgraber03@gmail.com>
2015-06-21 19:03:20 -0700

    Revert "added rm cmd for ota files to be copied"
    This reverts commit 3df242d200474d77d91d8fc5e6398ca11d1dacaa.

--- build-bp.sh
+++ build-bp.sh
@@ -46,6 +46,7 @@ normal='tput sgr0'
 
 tput bold
 tput setaf 1
+clear
 echo -e ""
 echo -e "BLISS POP"
 echo -e "Patrick's Device list"
@@ -55,6 +56,16 @@ echo -e ""
 
 sleep 3s
 
+
+# Confirm 'repo sync'
+echo -e "\n\n${bldgrn}  Do you want to repo sync before proceeding?\n"
+echo ""
+echo -e "${bldblu}  1. Yes"
+echo -e "${bldblu}  2. No"
+echo ""
+echo ""
+$normal
+read askreposync
 
 # Confirm 'make clean'
 echo -e "\n\n${bldgrn}  Do you want to make clean?\n"
@@ -66,8 +77,30 @@ echo ""
 $normal
 read askClean
 
+# Devices to build
+echo -e "\n\n${bldgrn}  Do you want to build all devices?\n"
+echo ""
+echo -e "${bldblu}  1. Yes"
+echo -e "${bldblu}  2. No"
 echo ""
 echo ""
+$normal
+read askdevices
+
+echo ""
+echo ""
+if [ "$askreposync" == "1" ]
+then
+    echo -e "${bldred}  Will repo sync before starting the build... "
+else
+    echo -e "${bldred}  Will continue without repo syncing... "
+fi
+echo ""
+echo ""
+
+echo ""
+echo ""
+
 if [ "$askClean" == "1" ]
 then
     echo -e "${bldred}  Compilation will continue after cleaning previous build files... "
@@ -77,43 +110,72 @@ fi
 echo ""
 echo ""
 
+echo ""
+echo ""
+if [ "$askdevices" == "1" ]
+then
+    echo -e "${bldred}  Will build Shamu, vs985, flo, deb, titan and Falcon... "
+else
+    echo -e "${bldred}  Will be just Shamu... "
+fi
+echo ""
+echo ""
 
 
 sleep 2s
 
 
+if [ "$askreposync" == "1" ]
+then
+	echo ""
+	echo ""
+	echo -e "${bldgrn}  syncing... "
+	echo ""
+	echo ""
+	$normal
+	repo sync -j250
+fi
 
 if [ "$askClean" == "1" ]
 then
 	echo ""
 	echo ""
-	echo -e "${bldgrn}  Removing files from previous compilations - Cleaning... "
+	echo -e "${bldgrn}  Cleaning before starting build... "
 	echo ""
 	echo ""
 	$normal
 	make clobber
 fi
 
-
-
-# Setup environment
-echo -e ""
-echo -e ""
-echo -e "${bldgrn}  Starting up build sequence for Shamu,vs985,flo,deb,titan and falcon..."
-echo -e ""
-echo -e ""
-$normal
-. build/envsetup.sh && brunch shamu && cp out/target/product/shamu/BlissPop* ~/blisshost && cp out/target/product/shamu/system/etc/CHANGELOG-bliss.txt ~/log && rm -rfv /home/pjgraber03/blisshost/ota3.xml && cp out/target/product/shamu/ota3.xml ~/blisshost && . build/envsetup.sh && brunch vs985 && cp out/target/product/vs985/BlissPop* /home/web-bliss/BlissPop/Official/vs985 && rm -rfv /home/web-bliss/BlissPop/Official/vs985/ota3.xml && cp out/target/product/vs985/ota3.xml /home/web-bliss/BlissPop/Official/vs985 && . build/envsetup.sh && brunch flo && cp out/target/product/flo/BlissPop* /home/web-bliss/BlissPop/Official/flo && rm -rfv /home/web-bliss/BlissPop/Official/flo/ota3.xml && cp out/target/product/flo/ota3.xml /home/web-bliss/BlissPop/Official/flo && . build/envsetup.sh && brunch deb && cp out/target/product/deb/BlissPop* /home/web-bliss/BlissPop/Official/deb && rm -rfv /home/web-bliss/BlissPop/Official/deb/ota.xml && cp out/target/product/deb/ota3.xml /home/web-bliss/BlissPop/Official/deb && . build/envsetup.sh && brunch titan && cp out/target/product/titan/BlissPop* /home/web-bliss/BlissPop/Official/titan && rm -rfv /home/web-bliss/BlissPop/Official/titan/ota3.xml && cp out/target/product/titan/ota3.xml /home/web-bliss/BlissPop/Official/titan && . build/envsetup.sh && brunch falcon && cp out/target/product/falcon/BlissPop* /home/web-bliss/BlissPop/Official/falcon && rm -rfv /home/web-bliss/BlissPop/Official/falcon/ota3.xml && cp out/target/product/falcon/ota3.xml /home/web-bliss/BlissPop/Official/falcon
+if [ "$askdevices" == "1" ]
+then
+	echo ""
+	echo ""
+	echo -e "${bldgrn}  Building all devices Now... "
+	echo ""
+	echo ""
+	$normal
+	. build/envsetup.sh && brunch shamu && cp out/target/product/shamu/BlissPop* ~/blisshost && cp out/target/product/shamu/system/etc/CHANGELOG-bliss.txt ~/log && cp out/target/product/shamu/ota3.xml ~/blisshost && . build/envsetup.sh && brunch vs985 && cp out/target/product/vs985/BlissPop* /home/web-bliss/BlissPop/Official/vs985 && cp out/target/product/vs985/ota3.xml /home/web-bliss/BlissPop/Official/vs985 && . build/envsetup.sh && brunch flo && cp out/target/product/flo/BlissPop* /home/web-bliss/BlissPop/Official/flo && cp out/target/product/flo/ota3.xml /home/web-bliss/BlissPop/Official/flo && . build/envsetup.sh && brunch deb && cp out/target/product/deb/BlissPop* /home/web-bliss/BlissPop/Official/deb && cp out/target/product/deb/ota3.xml /home/web-bliss/BlissPop/Official/deb && . build/envsetup.sh && brunch titan && cp out/target/product/titan/BlissPop* /home/web-bliss/BlissPop/Official/titan && cp out/target/product/titan/ota3.xml /home/web-bliss/BlissPop/Official/titan && . build/envsetup.sh && brunch falcon && cp out/target/product/falcon/BlissPop* /home/web-bliss/BlissPop/Official/falcon && cp out/target/product/falcon/ota3.xml /home/web-bliss/BlissPop/Official/falcon
+else 
+        echo ""
+        echo ""
+        echo -e "${bldgrn}  Building Shamu Now... "
+        echo ""
+        echo""
+        $normal
+        . build/envsetup.sh && brunch shamu && cp out/target/product/shamu/BlissPop* ~/blisshost && cp out/target/product/shamu/system/etc/CHANGELOG-bliss.txt ~/log && cp out/target/product/shamu/ota3.xml ~/blisshost
+fi
 
 
 
 # Compilation complete
 tput bold
 tput setaf 1
-clear
 echo -e ""
 echo -e "Device have all been built and uploaded"
 echo -e ""
 echo -e ""
 
 
+# Switch terminal back to normal
+$normal
